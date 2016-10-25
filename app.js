f4eae21580666cf6ab0d5215dd3e50fcfa31a2d9
Chris Jaffier <chrisd@Chriss-MBP.home>
2016-10-24 23:16:22 -0400

    search bar created

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let superData = [
 {name: 'Batman', phonenumber: 3019119889, affiliation: "DC", img_url: "http://cartoonbros.com/batman/batman-2/"},
 {name: 'Superman', phonenumber: 3019119881, affiliation:"DC", img_url:"http://www.logospike.com/wp-content/uploads/2014/11/Superman_logo-2.png"},
 {name: 'Spiderman', phonenumber: 3019119888, affiliation:"Marvel", img_url:"http://www.printablee.com/postpic/2009/07/spider-man-logo-circle_144773.jpg"}
-
 ]
 
 angular.module("superApp", [])
@@ -10,4 +9,8 @@ angular.module("superApp", [])
 
 function superController () {
   this.supers = superData
+  this.showSuper = function() {
+    let supers = {name: this.newSuper }
+   
+  }
 }
